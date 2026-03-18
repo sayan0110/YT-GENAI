50b8eac80d04c6ca01ea2e48421922baff1324a1
Sayan Laptop <sayanmondal254842@gmail.com>
2026-03-18 23:31:12 +0530

    changes

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router'
+import login from ('../src/pages/LoginPage')
+
+function AppRoutes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// export default AppRoutes
+export const router = createBrowserRouter([])
